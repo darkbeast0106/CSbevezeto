f1dd4b55bd951921fdd092851469a2c5a36d7c8f
Budaházi Bence <budahazi.bence@petrik.hu>
2022-09-13 17:34:48 +0200

    1. feladat

--- CSbevezeto/Program.cs
+++ CSbevezeto/Program.cs
@@ -4,7 +4,19 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Feladat1();
+        }
+
+        private static void Feladat1()
+        {
+            Console.Write("Adja meg a medence átmérőjét (m): ");
+            double atmero = double.Parse(Console.ReadLine());
+            Console.Write("Adja meg a medence mélység (m): ");
+            double melyseg = double.Parse(Console.ReadLine());
+            double sugar = atmero / 2;
+            double alapTerulet = sugar * sugar * Math.PI;
+            double terfogat = alapTerulet * melyseg;
+            Console.WriteLine($"A medencébe {terfogat:0.000} köbméter víz fér.");
         }
     }
 }
