8966961e5a412e6578b8217c9963fbe206330093
Budaházi Bence <budahazi.bence@petrik.hu>
2022-09-13 17:53:28 +0200

    4. Feladat

--- CSbevezeto/Program.cs
+++ CSbevezeto/Program.cs
@@ -1,10 +1,32 @@
-﻿namespace CSbevezeto
+﻿using System;
+
+namespace CSbevezeto
 {
     class Program
     {
         public static void Main(string[] args)
         {
-            Feladat1();
+            //Feladat1();
+            Feladat4();
+        }
+
+        private static void Feladat4()
+        {
+            Console.Write("Adja meg a háromszög 'a' oldalát(cm): ");
+            double a = double.Parse(Console.ReadLine());
+            Console.Write("Adja meg a háromszög 'b' oldalát(cm): ");
+            double b = double.Parse(Console.ReadLine());
+            Console.Write("Adja meg a háromszög 'c' oldalát(cm): ");
+            double c = double.Parse(Console.ReadLine());
+            if ((a + b > c) && (a + c > b) && (b + c > a))
+            {
+                double kerulet = a + b + c;
+                Console.WriteLine("A háromszög kerülete {0} cm", kerulet);
+            }
+            else
+            {
+                Console.WriteLine("Hibás adatok");
+            }
         }
 
         private static void Feladat1()
